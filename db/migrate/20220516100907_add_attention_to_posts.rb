4346b9aa2866f5ba1ea3ea05743f1785f8472e4c
yukimark <mark2peace@gmail.com>
2022-05-17 12:55:04 +0000

    チェックボックス型変更

--- db/migrate/20220516100907_add_attention_to_posts.rb
+++ db/migrate/20220516100907_add_attention_to_posts.rb
@@ -1,5 +1,5 @@
 class AddAttentionToPosts < ActiveRecord::Migration[6.1]
   def change
-    add_column :posts, :attention, :integer
+    add_column :posts, :attention, :boolean, default: false, null: false
   end
 end
